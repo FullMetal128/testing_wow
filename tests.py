58daafb19a8680e586e4c32a0736b5088756064a
dima- <dima-demin-2016@mail.ru>
2025-02-21 19:42:12 +0300

    YEEEEEAAAAAAAAAAAH

--- tests.py
+++ tests.py
@@ -1,8 +1,8 @@
 from main import *
 import conftest
+URL = 'https://petstore.swagger.io/v2'
 
 def test_connect():
-
     assert get_info() == 'Success'
 
 def test_add_pet():
@@ -13,5 +13,12 @@ def test_get_pet(data):
     assert get_pet(228) == rezult
 
 
+def test_add_pet(data):
+    response = requests.post(f"{URL}/pet", json = data)
+    assert response.status_code == 200
+    assert response.json()['pet']['name'] == data['name']
+
+def test_get_pet(data):
+    None
 
 
